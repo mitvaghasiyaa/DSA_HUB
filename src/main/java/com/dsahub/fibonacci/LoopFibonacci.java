ffaefa47f0f61b05a0fced8d02220a1a3b7ab31e
mitvaghasiyaa <mitvaghasiya2121@gmail.com>
2025-04-19 15:09:24 +0530

    fibonacci-program

---
+++ src/main/java/com/dsahub/fibonacci/LoopFibonacci.java
@@ -0,0 +1,19 @@
+package com.dsahub.fibonacci;
+
+public class LoopFibonacci {
+
+    public static void main(String[] args) {
+        int prev2=0;
+        int prev1=1;
+
+        System.out.println(prev2);
+        System.out.println(prev1);
+        
+        for(int fibo=0; fibo<20; fibo++){
+            int newFibo=prev2+prev1;
+            System.out.println(newFibo);
+            prev2=prev1;
+            prev1=newFibo;
+        }
+    }
+}
---
+++ src/main/java/com/dsahub/fibonacci/RecursionFibonacci.java
@@ -0,0 +1,29 @@
+package com.dsahub.fibonacci;
+
+public class RecursionFibonacci {
+
+    public static int count=2;
+
+    public static void fibonacci(int pre1,int prev2){
+        if(count<=10){
+            int newFibo=prev2+pre1;
+            System.out.println(newFibo);
+            prev2=pre1;
+            pre1=newFibo;
+            count+=1;
+            fibonacci(pre1,prev2);
+        }else {
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        int prev2=0;
+        int prev1=1;
+
+        System.out.println(prev2);
+        System.out.println(prev1);
+
+        fibonacci(prev1,prev2);
+    }
+}
